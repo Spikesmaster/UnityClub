d2698200f36bd49d79295acdf6051a9085896243
Kyle <kylemcalinden@hotmail.com>
2020-06-25 15:44:04 +0100

    added my prototype

---
+++ Assets/Scripts/DronePayloadController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DronePayloadController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject droneDepotUI;
+    public GameObject largePayloadController, smallPayloadController;
+    void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+        droneDepotUI.SetActive(true);
+    }
+
+    public void LargePayload()
+    {
+        //droneDepotUI.SetActive(false);
+        largePayloadController.GetComponent<DroneTravel>().enabled = true;
+    }
+
+    public void SmallPayload()
+    {
+        //droneDepotUI.SetActive(false);
+        smallPayloadController.GetComponent<DroneTravel>().enabled = true;
+    }
+    
+}
---
+++ Assets/Scripts/DroneTravel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DroneTravel : MonoBehaviour
+{
+    public Transform droneSpawn;
+    public GameObject dronePrefab;
+    public GameObject destination;
+    private NavMeshAgent agent;
+    public float speedOfAgent;
+    public float payloadDropTime;
+    void Start()
+    {
+        //startingPosition =  gameObject.transform.position;//, gameObject.transform.rotation);
+        //Instantiate(dronePrefab, droneSpawn.position, droneSpawn.localRotation);
+        //dronePrefab.GetComponent<NavMeshAgent>().Warp(droneSpawn.position);
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speedOfAgent;
+        agent.destination = destination.transform.position;
+        
+        //agent.destination = startingPosition.position;
+    }
+
+    
+    void Update()
+    {
+        
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Invoke("PayloadDrop", payloadDropTime);
+        }
+    }
+
+    void PayloadDrop()
+    {
+        agent.destination = droneSpawn.transform.position;
+    }
+}
