5e8d1fadf050e5a40cc496a453b07338c33d0357
Kyle <kylemcalinden@hotmail.com>
2020-06-26 12:16:06 +0100

    The navigation of the drones kinda works

--- Assets/Scripts/DronePayloadController.cs
+++ Assets/Scripts/DronePayloadController.cs
@@ -11,77 +11,40 @@ public class DronePayloadController : MonoBehaviour
     public GameObject destinationUI;
     public GameObject volcanoDest, mountainDest, mainDomeDest, rainforestDest;
     private NavMeshAgent agent;
-    public float speedOfAgent;
+    public float smallDroneSpeed, largeDroneSpeed;
     // public float payloadDropTime;
     private bool destinationWanted = false;
-    public GameObject largePayloadController;
-    
-    // void Start()
-    // {
-    //     EventTrigger trigger = GetComponent<EventTrigger>();
-    //     EventTrigger.Entry entry = new EventTrigger.Entry();
-    //     entry.eventID = EventTriggerType.PointerDown;
-    //     entry.callback.AddListener((data) => { OnPointerDownDelegate((PointerEventData)data); });
-    //     trigger.triggers.Add();
-    // }    
-    void OnMouseDown()
-    {
-        if (destinationWanted == false)
-        {
-            if (gameObject.CompareTag("LargeDrone"))
-            {
-                Debug.Log("OnMouseDown");
-                droneDepotUI.SetActive(true);
-                regionName.text = "Slow but strong"; //Since we are now touching the drones, the area doesn't make sense anymore.
-            }
+    public GameObject largePayloadController, smallPayloadController;
+    private bool largeDroneChosen = true;
 
-            if (gameObject.CompareTag("SmallDrone"))
-            {
-                droneDepotUI.SetActive(true);
-                regionName.text = "Fast but weak";
-            }
-        }
-
-    //     if (destinationWanted == true)
-    //     {
-    //         if (gameObject.CompareTag("Volcanic"))
-    //         {
-    //             Debug.Log("Got here");
-                
-    //         }
-
-    //         // if (gameObject.CompareTag("Rainforest"))
-    //         // {
-    //         //     Debug.Log("Got here");
-    //         //     agent = GetComponent<NavMeshAgent>();
-    //         //     agent.speed = speedOfAgent;
-    //         //     agent.destination = destination.transform.position;
-    //         // }
-    //     }
-    }
-    
-
-    // public void OnPointerDownDelegate(PointerEventData data)
-    // {
-    //     Debug.Log("OnPointerDownDelegate called.");
-    // }
-
-    
-    // void Update() 
-    // {
-    //     Debug.Log("Destination wanted: " + destinationWanted);
-    //     // if (agent.remainingDistance <= agent.stoppingDistance)
-    //     // {
-    //     //     destinationWanted = false;
-    //     // }
-    //     agent = largePayloadController.GetComponent<NavMeshAgent>();
-    // }
-
-    public void DestinationSelection()
+    public void ResourceSelection()
     {
         droneDepotUI.SetActive(false);
         destinationUI.SetActive(true);
         destinationWanted = true;
+    }
+
+    
+    public void LargeDrone()
+    {
+        if (destinationWanted == false)
+        {
+            Debug.Log("OnMouseDown");
+            droneDepotUI.SetActive(true);
+            regionName.text = "Slow but strong"; //Since we are now touching the drones, the area doesn't make sense anymore.
+            largeDroneChosen = true;    
+        }
+    }
+
+    public void SmallDrone()
+    {
+        if (destinationWanted == false)
+        {
+            Debug.Log("OnMouseDown");
+            droneDepotUI.SetActive(true);
+            regionName.text = "Fast but weak"; //Since we are now touching the drones, the area doesn't make sense anymore.
+            largeDroneChosen = false;   
+        }
     }
 
     public void AIBitVolcano()
@@ -89,10 +52,21 @@ public class DronePayloadController : MonoBehaviour
         if (destinationWanted == true)
             {
                 destinationUI.SetActive(false);
-                agent = largePayloadController.GetComponent<NavMeshAgent>();
-                agent.speed = speedOfAgent;
-                agent.destination = volcanoDest.transform.position;
-                destinationWanted = false;
+                if (largeDroneChosen == true)
+                {
+                    agent = largePayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = largeDroneSpeed;
+                    agent.destination = volcanoDest.transform.position;
+                    destinationWanted = false;
+                }
+
+                if (largeDroneChosen == false)
+                {
+                    agent = smallPayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = smallDroneSpeed;
+                    agent.destination = volcanoDest.transform.position;
+                    destinationWanted = false;
+                }
             }
     }
 
@@ -101,10 +75,21 @@ public class DronePayloadController : MonoBehaviour
         if (destinationWanted == true)
             {
                 destinationUI.SetActive(false);
-                agent = largePayloadController.GetComponent<NavMeshAgent>();
-                agent.speed = speedOfAgent;
-                agent.destination = mountainDest.transform.position;
-                destinationWanted = false;
+                if (largeDroneChosen == true)
+                {
+                    agent = largePayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = largeDroneSpeed;
+                    agent.destination = mountainDest.transform.position;
+                    destinationWanted = false;
+                }
+
+                if (largeDroneChosen == false)
+                {
+                    agent = smallPayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = smallDroneSpeed;
+                    agent.destination = mountainDest.transform.position;
+                    destinationWanted = false;
+                }
             }
     }
 
@@ -113,10 +98,21 @@ public class DronePayloadController : MonoBehaviour
         if (destinationWanted == true)
             {
                 destinationUI.SetActive(false);
-                agent = largePayloadController.GetComponent<NavMeshAgent>();
-                agent.speed = speedOfAgent;
-                agent.destination = rainforestDest.transform.position;
-                destinationWanted = false;
+                if (largeDroneChosen == true)
+                {
+                    agent = largePayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = largeDroneSpeed;
+                    agent.destination = rainforestDest.transform.position;
+                    destinationWanted = false;
+                }
+
+                if (largeDroneChosen == false)
+                {
+                    agent = smallPayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = smallDroneSpeed;
+                    agent.destination = rainforestDest.transform.position;
+                    destinationWanted = false;
+                }
             }
     }
 
@@ -125,10 +121,21 @@ public class DronePayloadController : MonoBehaviour
         if (destinationWanted == true)
             {
                 destinationUI.SetActive(false);
-                agent = largePayloadController.GetComponent<NavMeshAgent>();
-                agent.speed = speedOfAgent;
-                agent.destination = mainDomeDest.transform.position;
-                destinationWanted = false;
+                if (largeDroneChosen == true)
+                {
+                    agent = largePayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = largeDroneSpeed;
+                    agent.destination = mainDomeDest.transform.position;
+                    destinationWanted = false;
+                }
+
+                if (largeDroneChosen == false)
+                {
+                    agent = smallPayloadController.GetComponent<NavMeshAgent>();
+                    agent.speed = smallDroneSpeed;
+                    agent.destination = mainDomeDest.transform.position;
+                    destinationWanted = false;
+                }
             }
     }
 }
--- Assets/Scripts/PauseMenu2.cs
+++ Assets/Scripts/PauseMenu2.cs
@@ -1,31 +1,33 @@
-﻿// using System;
-// using UnityEngine;
-// public class PauseMenu2 : MonoBehaviour
-// {
-//     public event Action<bool>  PauseToggleEvent;
-//     bool isPaused;
-//     [SerializeField]
-//     KeyCode pauseKey;
+﻿using System;
+using UnityEngine;
+public class PauseMenu2 : MonoBehaviour
+{
+    public event Action<bool>  PauseToggleEvent;
+    bool isPaused;
+    public GameObject pausePanel;
+    [SerializeField]
+    public KeyCode pauseKey;
 
-//     void Start ()
-//     { 
-//         isPaused = false;
-//     }
-//     void Update()
-//     {
-//         ListenForPauseInput();
-//     }
-//     void ListenForPauseInput()
-//     {
-//         if (Input.GetKeyDown(pauseKey)
-//         {
-//             TogglePause();
-//         }
-//     }
+    void Start ()
+    { 
+        isPaused = false;
+    }
+    void Update()
+    {
+        ListenForPauseInput();
+    }
+    void ListenForPauseInput()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            TogglePause();
+        }
+    }
 
-//     void TogglePause()
-//     {
-//         isPaused != isPaused;
-//         PauseToggleEvent?. Invoke(isPaused);
-//     }
-// }
+    void TogglePause()
+    {
+        //isPaused != isPaused;
+        //PauseToggleEvent?. Invoke(isPaused);
+        pausePanel.SetActive(true);
+    }
+}
