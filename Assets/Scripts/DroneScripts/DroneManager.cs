be5e5fa621cbd697fbfe9a4bf66e060fa85f6d9e
Victor <spikesmaster@gmail.com>
2020-06-29 16:16:33 +0100

    Drone movement fix

---
+++ Assets/Scripts/DroneScripts/DroneManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class DroneManager : MonoBehaviour
+{
+
+    public GameObject droneDepotUI;
+    public TextMeshProUGUI regionNameUI;
+    public string regionNameString;
+    public GameObject destinationUI;
+    public float droneSpeed;
+    public bool destinationWanted = false;
+    
+    public bool isInTransit = false;
+    public void NewDroneSelected ()
+    {
+        if (!destinationWanted && !isInTransit) 
+        {
+            droneDepotUI.SetActive(true);
+            regionNameUI.text = regionNameString;
+            
+        }
+    }
+
+    public float destinationDistanceOffset;
+    public void SelectDroneDestination(GameObject outpostDestination){
+        
+        if (destinationWanted == true)
+        {
+            destinationUI.SetActive(false);
+            NavMeshAgent agent = transform.GetComponent<NavMeshAgent>();
+            agent.speed = droneSpeed;
+            agent.destination = outpostDestination.transform.position;
+            agent.stoppingDistance = destinationDistanceOffset;
+            isInTransit = true;
+        }
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        if(transform.GetComponent<NavMeshAgent>().remainingDistance<=destinationDistanceOffset&&isInTransit){
+            destinationWanted = false;
+            isInTransit = false;
+            Debug.Log("test");
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/DroneScripts/DroneMovementManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovementManager : MonoBehaviour
+{
+    public GameObject currentDroneSelected;
+    public void NewDestinationSelected(GameObject outpostDestination){
+
+        currentDroneSelected.GetComponent<DroneManager>().SelectDroneDestination(outpostDestination);
+
+    }
+
+    public void SetNewCurrentDrone(GameObject newDrone){
+
+        currentDroneSelected = newDrone;
+
+    }
+
+
+    public GameObject droneDepotUI;
+    public GameObject destinationUI;
+    public void ResourceSelection()
+    {
+        droneDepotUI.SetActive(false);
+        destinationUI.SetActive(true);
+        currentDroneSelected.GetComponent<DroneManager>().destinationWanted = true;
+    }
+}
